73db4f6f379f6239e27928bba790cead0c6286d0
Matas Ramašauskas <public@ramasauskas.com>
2024-10-28 19:51:09 +0200

    use proper dsn

--- cmd/bryant/main.go
+++ cmd/bryant/main.go
@@ -7,16 +7,20 @@ import (
 	"os"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ssm"
 	"github.com/cristalhq/aconfig"
 	"github.com/jackc/pgx/v5"
 )
 
-var config struct {
+var envCfg struct {
 	DBDSN string `env:"DB_DSN"`
 }
 
+var dbDSN string
+
 func init() {
-	loader := aconfig.LoaderFor(&config, aconfig.Config{
+	loader := aconfig.LoaderFor(&envCfg, aconfig.Config{
 		// feel free to skip some steps :)
 		// SkipDefaults: true,
 		// SkipFiles:    true,
@@ -31,11 +35,33 @@ func init() {
 		os.Exit(1)
 	}
 
-	slog.Info("connected")
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		slog.Error("loading default config", slog.Any("error", err))
+
+		os.Exit(1)
+	}
+
+	client := ssm.NewFromConfig(cfg)
+
+	name := "/sbd/dev/scoutapi/db-dsn"
+	decrypt := true
+
+	o, err := client.GetParameter(context.Background(), &ssm.GetParameterInput{
+		Name:           &name,
+		WithDecryption: &decrypt,
+	})
+	if err != nil {
+		slog.Error("getting parameter", slog.Any("error", err))
+
+		os.Exit(1)
+	}
+
+	dbDSN = *o.Parameter.Value
 }
 
 func handle(ctx context.Context, _ json.RawMessage) error {
-	conn, err := pgx.Connect(ctx, config.DBDSN)
+	conn, err := pgx.Connect(ctx, dbDSN)
 	if err != nil {
 		slog.Error("connecting to db", slog.Any("error", err))
 
@@ -43,6 +69,8 @@ func handle(ctx context.Context, _ json.RawMessage) error {
 	}
 
 	defer conn.Close(context.Background())
+
+	slog.Info("connected")
 
 	return nil
 }
--- go.mod
+++ go.mod
@@ -4,10 +4,25 @@ go 1.23.0
 
 require (
 	github.com/aws/aws-lambda-go v1.47.0 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.32.2 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.28.0 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.17.41 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.17 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.21 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.21 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.12.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.12.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssm v1.55.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.24.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.28.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.32.2 // indirect
+	github.com/aws/smithy-go v1.22.0 // indirect
 	github.com/cristalhq/aconfig v0.18.6 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.7.1 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	golang.org/x/crypto v0.27.0 // indirect
 	golang.org/x/text v0.18.0 // indirect
